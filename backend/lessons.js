d87acf45bf628dd59486e0087417b43dede853e4
Rian Qadir <rianqadir2011@gmail.com>
2024-12-02 01:35:03 +0500

    update

--- backend/lessons.js
+++ backend/lessons.js
@@ -1,4 +1,4 @@
-lessons: [
+const lessons = [
     { "id": 1, "subject": "Maths", "location": "London", "price": 20, "spaces": 5, "image": "backend/static/images/maths.png", "addedToCart": 0, "rating": 4 },
     { "id": 2, "subject": "English", "location": "Birmingham", "price": 25, "spaces": 5, "image": "backend/static/images/english.png", "addedToCart": 0, "rating": 3 },
     { "id": 3, "subject": "Science", "location": "Manchester", "price": 30, "spaces": 5, "image": "backend/static/images/science.png", "addedToCart": 0, "rating": 5 },
@@ -9,4 +9,4 @@ lessons: [
     { "id": 8, "subject": "Chemistry", "location": "Glasgow", "price": 32, "spaces": 5, "image": "backend/static/images/chemistry.png", "addedToCart": 0, "rating": 5 },
     { "id": 9, "subject": "Physics", "location": "Edinburgh", "price": 28, "spaces": 5, "image": "backend/static/images/physics.png", "addedToCart": 0, "rating": 4 },
     { "id": 10, "subject": "Biology", "location": "Cardiff", "price": 24, "spaces": 5, "image": "backend/static/images/biology.png", "addedToCart": 0, "rating": 3 }
-]
+];
